b7488e62ead382059a6eec11f3361270a8a97c12
SibsanSuk <60909299+SibsanSuk@users.noreply.github.com>
2021-10-24 14:22:52 +0700

    เพิ่ม Unity Project

---
+++ KUKAfternoon/Assets/Scripts/HomeSceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HomeSceneManager : MonoBehaviour
+{
+    public void GoToPlayground()
+    {
+        //PlayerPrefs.SetInt("CoinCount", 0);
+        PlayerPrefs.DeleteKey("CoinCount");
+        SceneManager.LoadScene("Playground");
+    }
+
+    public void ContinuePlay()
+    {
+        if (PlayerPrefs.HasKey("PrevScene"))
+        {
+            string prevSceneName = PlayerPrefs.GetString("PrevScene");
+            SceneManager.LoadScene(prevSceneName);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ KUKAfternoon/Assets/Scripts/LightController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light light1;
+    public float intensity = 1f;
+    private void OnMouseDown()
+    {
+        light1.enabled = !light1.enabled;
+        light1.intensity = intensity;
+    }
+}
+
